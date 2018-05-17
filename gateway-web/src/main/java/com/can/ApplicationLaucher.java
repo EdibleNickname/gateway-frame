eb3d5f48d31c337327b0af12a96ec6268eafac20
LCN <1455476411@qq.com>
2018-05-17 20:28:23 +0800

    提交controller层和service层

---
+++ gateway-web/src/main/java/com/can/ApplicationLaucher.java
@@ -0,0 +1,13 @@
+package com.can;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLaucher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLaucher.class, args);
+	}
+
+}
