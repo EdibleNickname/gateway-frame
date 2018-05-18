c62a50499138276a0ca9408f91c5507334e6e201
LCN <1455476411@qq.com>
2018-05-18 11:53:08 +0800

    增加全局捕获异常

--- gateway-web/src/main/java/com/can/ApplicationLaucher.java
+++ gateway-web/src/main/java/com/can/ApplicationLaucher.java
@@ -3,10 +3,19 @@ package com.can;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @description: 程序启动类
+ *
+ * @author: LCN
+ * @date: 2018-05-17 21:10
+ */
+
 @SpringBootApplication
 public class ApplicationLaucher {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(ApplicationLaucher.class, args);
 	}
 
