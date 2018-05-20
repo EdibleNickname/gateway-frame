9ee93b2c902fc02699796c85bfdc9c64ad3be8a8
LCN <1455476411@qq.com>
2018-05-20 21:43:57 +0800

    整体结构完成

---
+++ gateway-core/gateway-provider-impl/src/main/java/com/can/App.java
@@ -0,0 +1,13 @@
+package com.can;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
